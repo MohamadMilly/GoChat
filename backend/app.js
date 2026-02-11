9db02c54bee7ce31449938cbf10e97dee53bd30c
MohamadMilly <volassoa@gmail.com>
2026-02-11 14:51:43 +0300

    removed credentials from cors config

--- backend/app.js
+++ backend/app.js
@@ -173,8 +173,7 @@ app.use(
   cors({
     origin: process.env.CLIENT_URL,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
+    allowedHeaders: ["Content-Type", "authorization"],
   }),
 );
 
