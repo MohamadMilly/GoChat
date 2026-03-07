78a93a7263c53b6ce3a49e69d0ac4694001ea7df
MohamadMilly <volassoa@gmail.com>
2026-03-07 16:21:02 +0300

    memoized the ChatEntry not to rerender when a message sent or recieved

--- frontend/GoChat/src/components/chatList/ChatEntry.jsx
+++ frontend/GoChat/src/components/chatList/ChatEntry.jsx
@@ -1,103 +1,105 @@
 import { printGroupTypingUsers } from "../../utils/printGroupTypingUsers";
 import { Avatar } from "../chat/Avatar";
 import { NavLink } from "react-router";
-import { useEffect, useState } from "react";
+import { memo, useEffect, useState } from "react";
 import { socket } from "../../socket";
 import { ChatBubbleStatus } from "../chat/chatBubbleStatus";
 
-export function ChatEntry({
-  chatAvatar,
-  isGroup,
-  chatTitle,
-  lastMessage = null,
-  onOpenChat,
-  isConnected,
-  typingUsers,
-  conversationId,
-  color,
-}) {
-  const base_class =
-    "w-full flex items-center gap-x-3 p-3 hover:bg-gray-50 dark:hover:bg-gray-800 transition-colors duration-150";
-  const initialReadersIds = lastMessage.readers
-    ? lastMessage.readers.map((reader) => reader.readerId)
-    : [];
-  const [readers, setReaders] = useState(initialReadersIds);
+export const ChatEntry = memo(
+  ({
+    chatAvatar,
+    isGroup,
+    chatTitle,
+    lastMessage = null,
+    onOpenChat,
+    isConnected,
+    typingUsers,
+    conversationId,
+    color,
+  }) => {
+    const base_class =
+      "w-full flex items-center gap-x-3 p-3 hover:bg-gray-50 dark:hover:bg-gray-800 transition-colors duration-150";
+    const initialReadersIds = lastMessage.readers
+      ? lastMessage.readers.map((reader) => reader.readerId)
+      : [];
+    const [readers, setReaders] = useState(initialReadersIds);
 
-  useEffect(() => {
-    function onReadMessage(messageId, readerId) {
-      if (messageId === lastMessage?.id) {
-        setReaders((prev) => [...prev, readerId]);
+    useEffect(() => {
+      function onReadMessage(messageId, readerId) {
+        if (messageId === lastMessage?.id) {
+          setReaders((prev) => [...prev, readerId]);
+        }
       }
-    }
 
-    socket.on("read message", onReadMessage);
+      socket.on("read message", onReadMessage);
 
-    return () => socket.off("read message", onReadMessage);
-  });
+      return () => socket.off("read message", onReadMessage);
+    });
 
-  return (
-    <li>
-      <button className="w-full" onClick={onOpenChat}>
-        <NavLink
-          viewTransition
-          to={`/chats/${isGroup ? "group" : "direct"}/${conversationId}`}
-          className={({ isActive, isPending }) => {
-            if (isActive) {
-              return `${base_class} bg-cyan-600/40 dark:bg-cyan-400/40`;
-            }
-            if (isPending) {
-              return `${base_class} bg-gray-100`;
-            }
-            return base_class;
-          }}
-        >
-          <div className="shrink-0 relative">
-            <Avatar avatar={chatAvatar} chatTitle={chatTitle} color={color} />
-            {!isGroup && (
-              <span
-                className={`absolute w-3 h-3 bottom-0 right-0 rounded-full ${isConnected ? "bg-cyan-600 dark:bg-cyan-400" : "bg-gray-400 dark:bg-gray-300"}`}
-              ></span>
-            )}
-          </div>
-
-          <div className="flex-1 min-w-0">
-            <div className="flex items-center justify-between gap-x-2">
-              <strong className="text-gray-900 dark:text-gray-50 truncate">
-                {chatTitle}
-              </strong>
-              <span className="text-xs text-gray-400 dark:text-gray-200">
-                {lastMessage?.createdAt
-                  ? new Date(lastMessage.createdAt).toLocaleTimeString()
-                  : ""}
-              </span>
+    return (
+      <li>
+        <button className="w-full" onClick={onOpenChat}>
+          <NavLink
+            viewTransition
+            to={`/chats/${isGroup ? "group" : "direct"}/${conversationId}`}
+            className={({ isActive, isPending }) => {
+              if (isActive) {
+                return `${base_class} bg-cyan-600/40 dark:bg-cyan-400/40`;
+              }
+              if (isPending) {
+                return `${base_class} bg-gray-100`;
+              }
+              return base_class;
+            }}
+          >
+            <div className="shrink-0 relative">
+              <Avatar avatar={chatAvatar} chatTitle={chatTitle} color={color} />
+              {!isGroup && (
+                <span
+                  className={`absolute w-3 h-3 bottom-0 right-0 rounded-full ${isConnected ? "bg-cyan-600 dark:bg-cyan-400" : "bg-gray-400 dark:bg-gray-300"}`}
+                ></span>
+              )}
             </div>
-            <p className="text-gray-600 dark:text-gray-200 text-sm  text-left mt-1 flex items-center justify-between">
-              {typingUsers.length > 0 ? (
-                isGroup ? (
-                  <span className="truncate">
-                    {printGroupTypingUsers(typingUsers)}
-                  </span>
-                ) : (
-                  <span>typing...</span>
-                )
-              ) : (
-                <span className="truncate">
-                  {lastMessage ? lastMessage.content : ""}
+
+            <div className="flex-1 min-w-0">
+              <div className="flex items-center justify-between gap-x-2">
+                <strong className="text-gray-900 dark:text-gray-50 truncate">
+                  {chatTitle}
+                </strong>
+                <span className="text-xs text-gray-400 dark:text-gray-200">
+                  {lastMessage?.createdAt
+                    ? new Date(lastMessage.createdAt).toLocaleTimeString()
+                    : ""}
                 </span>
-              )}
-              {lastMessage && (
-                <ChatBubbleStatus
-                  readers={readers}
-                  senderId={lastMessage?.sender?.id || lastMessage.senderId}
-                  status={lastMessage.status}
-                  className={"shrink-0"}
-                  supportNotifications={true}
-                />
-              )}
-            </p>
-          </div>
-        </NavLink>
-      </button>
-    </li>
-  );
-}
+              </div>
+              <p className="text-gray-600 dark:text-gray-200 text-sm  text-left mt-1 flex items-center justify-between">
+                {typingUsers.length > 0 ? (
+                  isGroup ? (
+                    <span className="truncate">
+                      {printGroupTypingUsers(typingUsers)}
+                    </span>
+                  ) : (
+                    <span>typing...</span>
+                  )
+                ) : (
+                  <span className="truncate">
+                    {lastMessage ? lastMessage.content : ""}
+                  </span>
+                )}
+                {lastMessage && (
+                  <ChatBubbleStatus
+                    readers={readers}
+                    senderId={lastMessage?.sender?.id || lastMessage.senderId}
+                    status={lastMessage.status}
+                    className={"shrink-0"}
+                    supportNotifications={true}
+                  />
+                )}
+              </p>
+            </div>
+          </NavLink>
+        </button>
+      </li>
+    );
+  },
+);
