e1b53403f62565c60cecc28cff841f81580fac7c
MohamadMilly <volassoa@gmail.com>
2026-03-11 13:47:31 +0300

    added basis-full to the main element in the chat list page component

--- frontend/GoChat/src/routes/ChatsListPage.jsx
+++ frontend/GoChat/src/routes/ChatsListPage.jsx
@@ -30,7 +30,7 @@ export function ChatsListPage() {
         <SideDrawer />
         <ChatsListHeader />
         <main
-          className={`flex-1 w-full md:grid  ${isChatsPanelCollapsed ? "md:grid-cols-[1fr]" : language === "Arabic" ? "md:grid-cols-[1fr_350px]" : "md:grid-cols-[350px_1fr]"} h-[calc(100vh-64px)]`}
+          className={`flex-1 basis-full w-full md:grid  ${isChatsPanelCollapsed ? "md:grid-cols-[1fr]" : language === "Arabic" ? "md:grid-cols-[1fr_350px]" : "md:grid-cols-[350px_1fr]"} h-[calc(100vh-64px)]`}
         >
           <ChatsPanel />
           <Outlet />
