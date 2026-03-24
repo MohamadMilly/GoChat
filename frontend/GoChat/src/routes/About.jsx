efa21768fe5d5f3540c9fb157dc6fc138b32e7c1
MohamadMilly <volassoa@gmail.com>
2026-03-24 15:20:57 +0300

    redisgned about page

--- frontend/GoChat/src/routes/About.jsx
+++ frontend/GoChat/src/routes/About.jsx
@@ -6,24 +6,28 @@ export function About() {
   const { language } = useLanguage();
   const t = translations.AboutPage;
   return (
-    <main className="max-w-200 mx-auto p-6">
-      <h2 className="font-rubik text-4xl text-cyan-600 font-bold text-center my-12">
+    <main
+      className="max-w-200 mx-auto p-6  bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] 
+             dark:bg-[radial-gradient(#1e293b_1px,transparent_1px)] 
+             [background-size:16px_16px] h-full border-x border-[#e5e7eb] dark:border-[#1e293b]"
+    >
+      <h2 className="font-rubik sm:text-4xl text-3xl text-cyan-600 dark:text-cyan-400 font-bold text-center my-8 sm:my-12">
         {t[language].Title}
       </h2>
       <article>
-        <p className="text-lg text-gray-700 dark:text-gray-200">
+        <p className="sm:text-lg text-base text-gray-700 dark:text-gray-200 text-balance text-center">
           {t[language].Body}
         </p>
-        <aside className="flex justify-between w-full max-w-120 mx-auto shrink-0 mt-6 border-2 border-cyan-500 rounded-md p-3">
+        <aside className="flex justify-between w-full max-w-120 mx-auto shrink-0 mt-8 border border-cyan-600 dark:border-cyan-400 rounded-full overflow-hidden">
           <Link
-            route={"https://github.com/MohamadMilly"}
-            className="text-cyan-600 text-sm md:text-base rounded"
+            route={"https://github.com"}
+            className="text-gray-700 text-center grow p-3! text-xs md:text-base bg-gray-50"
           >
             {t[language].Github}
           </Link>
           <Link
-            route={"https://t.me/Mohamadmilly"}
-            className="text-cyan-600 text-sm md:text-base rounded"
+            route={"https://t.me"}
+            className="text-gray-700 text-xs text-center p-3! grow md:text-base bg-gray-50"
           >
             {t[language].Telegram}
           </Link>
