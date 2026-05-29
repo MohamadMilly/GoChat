113c1c71d1ec044aba12d5242299b8ad8ed9f5a4
MohamadMilly <volassoa@gmail.com>
2026-05-29 08:42:51 +0300

    made the stickerspanel beatiful for light mode too

--- frontend/GoChat/src/components/chat/sendMessageForm/StickersPanel.jsx
+++ frontend/GoChat/src/components/chat/sendMessageForm/StickersPanel.jsx
@@ -98,9 +98,9 @@ export function StickersPanel({ open, setOpen }) {
   return (
     <aside
       ref={stickersPanelRef}
-      className={`pt-6 md:-mb-4 -mb-2 px-2 [--slide-offset:100%] overflow-y-auto  w-full border-gray-800 border backdrop-blur-md border-dashed bg-linear-to-t from-gray-800 to-gray-900/80 absolute z-900 bottom-0 h-90 rounded-t-lg shadow-lg ${isSlidingDown ? "animate-slidedown" : "animate-slideup"}`}
+      className={`pt-6 pb-4 md:-mb-4 -mb-2 px-2 [--slide-offset:100%] overflow-y-auto  w-full dakr:border-gray-800 border backdrop-blur-md border-dashed bg-linear-to-t dark:from-gray-800 dark:to-gray-900/ border-gray-700/40 from-gray-100 to-gray-50 absolute z-900 bottom-0 h-90 rounded-t-lg shadow-lg ${isSlidingDown ? "animate-slidedown" : "animate-slideup"}`}
     >
-      <span className="absolute left-[50%] translate-x-[-50%] top-0 translate-y-[-50%]  w-8 h-2 dark:bg-gray-600 rounded-full"></span>
+      <span className="absolute left-[50%] translate-x-[-50%] top-0 translate-y-[-50%]  w-8 h-2 dark:bg-gray-600 bg-gray-700 rounded-full"></span>
       <StickersContent
         isFetching={isFetching}
         stickers={stickers}
